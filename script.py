48763f0215b14068b692d4b9c09e3f91fd2f11a5
Thiago Eugênio <65611648+eugeniothiago@users.noreply.github.com>
2022-03-28 15:50:31 -0300

    adjusted script
    added some folder verification for preventing errors.

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from tokenize import Name
 import pandas as pd
 import numpy as np
 import re
@@ -38,54 +39,63 @@ folder = f'{project_dir}/{folder_list[int(user_input)]}'
 
 # lẽ todos os arquivos .zip e descompacta na pasta raw_data:
 print(f'Descompactando os arquivos .zip contidos na pasta {folder}:')
+time.sleep(1)
 raw_data_list = [data for data in os.listdir(folder)]
 
 if not os.path.exists('fee_data'):
     os.mkdir('fee_data')
-        
-for archive in raw_data_list:
-    if not raw_data_list:
+
+if not raw_data_list:
         print('Não há nenhum arquivo .zip na pasta raw_data. O script será encerrado.')
         sys.exit()
-    else:
+else:
+    for archive in raw_data_list:
         name, extension = os.path.splitext(archive)
         if extension =='.zip':
             with ZipFile(f'{folder}/{archive}','r') as zipfile:
                 zipfile.extractall(f'{project_dir}/fee_data/')
-del archive, name, extension, raw_data_list
 gc.collect()
-
-
-print('Descompactação concluída. Tratando os dados...')
 
 if not os.path.exists('fee_melted_data'):
         os.mkdir('fee_melted_data')
 
-#expandindo e formatando os arquivos que foram compactados:
-for file in os.listdir(f'{project_dir}/fee_data/'):
-    data = pd.read_csv(f'{project_dir}/fee_data/{file}',sep='\n', encoding='latin-1', header=None)
-    data = data[0].str.split(';', expand=True)
-    data = data.reset_index(drop=True)
-    var_name = data[0][1][7:]
-    var_name = unidecode(var_name).lower().strip().replace(' ','_').replace('/','_').replace(':','')
-    data.columns = data.iloc[5].tolist()
-    data = data.drop([0,1,2,3,4,5], axis=0).reset_index(drop=True)
-    data = data.drop(columns=['latitude','longitude'])
-    for column in data.columns[2:]:
-        column_name = re.sub(r'\D*','',column)
-        data = data.rename(columns={column:f'{column_name}'})
-    
-    data_melted = data.melt(id_vars=['Município','ibge'], var_name=f'ano', value_name=f'{var_name}_value')
-    data_melted.to_csv(f'{project_dir}/fee_melted_data/{var_name}_value.csv', sep=';', decimal=',', encoding='latin-1', index=False)
-del data, data_melted, file, var_name
+data_list = os.listdir(f'{project_dir}/fee_data/')
+
+if not data_list:
+    print('Nenhum arquivo a ser processado.')
+    sys.exit()
+else:
+    for file in os.listdir(f'{data_list}'):
+        if not file:
+            print('Não há arquivos processados. O script será encerrado.')
+            sys.exit()
+        data = pd.read_csv(f'{project_dir}/fee_data/{file}',sep='\n', encoding='latin-1', header=None)
+        data = data[0].str.split(';', expand=True)
+        data = data.reset_index(drop=True)
+        var_name = data[0][1][7:]
+        var_name = unidecode(var_name).lower().strip().replace(' ','_').replace('/','_').replace(':','')
+        data.columns = data.iloc[5].tolist()
+        data = data.drop([0,1,2,3,4,5], axis=0).reset_index(drop=True)
+        data = data.drop(columns=['latitude','longitude'])
+        for column in data.columns[2:]:
+            column_name = re.sub(r'\D*','',column)
+            data = data.rename(columns={column:f'{column_name}'})
+        
+        data_melted = data.melt(id_vars=['Município','ibge'], var_name=f'ano', value_name=f'{var_name}_value')
+        data_melted.to_csv(f'{project_dir}/fee_melted_data/{var_name}_value.csv', sep=';', decimal=',', encoding='latin-1', index=False)
 gc.collect()
 
 #criando o dataset inicial e realizando o merge com os dados disponíveis
 file_list = [file for file in os.listdir(f'{project_dir}/fee_melted_data/')]
-melted_data = pd.read_csv(f'{project_dir}/fee_melted_data/{file_list[0]}', header=0, sep=';',encoding='latin-1')
-#melted_data.columns = melted_data.iloc[0]
-#melted_data = melted_data.drop([0], axis=0)
-melted_data = melted_data.astype({'ibge':'str','ano':'str'})
+if file_list:
+    melted_data = pd.read_csv(f'{project_dir}/fee_melted_data/{file_list[0]}', header=0, sep=';',encoding='latin-1')
+    #melted_data.columns = melted_data.iloc[0]
+    #melted_data = melted_data.drop([0], axis=0)
+    melted_data = melted_data.astype({'ibge':'str','ano':'str'})
+else:
+    print('Não há dados a serem processados. O script será encerrado.')
+    time.sleep(1)
+    sys.exit()
 
 for file in file_list[1:]:
     df= pd.read_csv(f'{project_dir}/fee_melted_data/{file}', header=0, sep=';', encoding='latin-1')
